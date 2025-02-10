da8a9a0f2e2bd7886439377da70346f3dc5310e4
AlcheMystic <pashkinms86@gmail.com>
2025-02-11 00:38:58 +0700

    first commit

---
+++ templates/bootstrap_template/js/script.js
@@ -0,0 +1,7 @@
+const myCarouselElement = 
+document.querySelector('#carouselExampleIndicators')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 5000,
+    touch: false
+})
